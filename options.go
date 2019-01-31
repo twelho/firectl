bf7e426d0154264ec3c575703e96fe95a12adc33
Lucas Käldström, NYLBR <lucas@luxaslabs.com>
2019-01-31 13:17:04 +0200

    Clean up the runtime dir afterwards

--- options.go
+++ options.go
@@ -206,10 +206,15 @@ func (opts *options) ToVMM() (*VMM, error) {
 		return nil, err
 	}
 
-	// Create the runtime directory for the VM. TODO: Should we clean it up directly?
-	if err := os.MkdirAll(filepath.Join(RuntimeDir, opts.Name), 0755); err != nil {
+	// Create the runtime directory for the VM.
+	vmdir := filepath.Join(RuntimeDir, opts.Name)
+	if err := os.MkdirAll(vmdir, 0755); err != nil {
 		return nil, err
 	}
+	// After execution, we can clean it up
+	opts.addCleanupFn(func() error {
+		return os.RemoveAll(vmdir)
+	})
 
 	cfg := firecracker.Config{
 		// FifoLogWriter will be set based on opts.FifoLogFile later during runtime
